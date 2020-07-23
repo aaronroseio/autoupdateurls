f3069b90fb9e672bc1676201e1367b2b73726791
aaronroseio <68356667+aaronroseio@users.noreply.github.com>
2020-07-23 09:37:15 -0400

    Update updatesiteURLs.py

--- updatesiteURLs.py
+++ updatesiteURLs.py
@@ -21,7 +21,7 @@ installTargets = 'R8040GWDEVTEST'  # policy-install-targets-here
 
 def retrieveLatest():
     filedata = urllib2.urlopen(
-        'https://raw.githubusercontent.com/aaronrose18/susdom/master/suspiciousdomains.txt', context=ctx)
+        'https://github.com/aaronroseio/autoupdateurls/blob/master/suspiciousdomains.txt', context=ctx)
     datatowrite = filedata.read()
 
     with open('suspiciousdomains.txt', 'wb') as f:
